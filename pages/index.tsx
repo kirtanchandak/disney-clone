3e73ba41c1204cb490323c311e7600c198eb2251
gdscghrcem <themarketingkirtan@gmail.com>
2022-11-24 22:13:03 +0530

    header build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Header from "../components/Header"
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Disney+ Hotstar</h1>
+      <Header/>
     </div>
   )
 }
